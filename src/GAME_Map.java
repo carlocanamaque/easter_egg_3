444ee976a6811029ed914b5ebd61057bf095f817
carlocanamaque <carlocanamaque@proton.me>
2025-07-23 04:30:35 +0800

    Began progress on GAME_Map.java

--- src/GAME_Map.java
+++ src/GAME_Map.java
@@ -1,1 +1,21 @@
 package src;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+
+public class GAME_Map {
+
+	GAME_Processor processor;
+	PARENT_Obstacle[] obstacles = new PARENT_Obstacle[2];
+
+	public GAME_Map(GAME_Processor processor) {
+
+		this.processor = processor;
+	}
+	void draw(Graphics2D g2) { 
+
+		// TO-DO: code here...
+	}
+}
