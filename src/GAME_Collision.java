05bceed9962b095dbb0f0546190637e21fe41b2c
carlocanamaque <carlocanamaque@proton.me>
2025-07-24 07:50:00 +0800

    Instantiated GAME_Collision.java

---
+++ src/GAME_Collision.java
@@ -0,0 +1,33 @@
+package src;
+
+
+public class GAME_Collision {
+
+	GAME_Processor processor;
+	ENTITY_Player player;
+	GAME_Map map;
+	GAME_Hostiles hostiles;
+
+	public GAME_Collision(GAME_Processor processor) {
+
+		this.processor = processor;
+		this.player = processor.player;
+		this.map = processor.map;
+		this.hostiles = processor.hostiles;
+	}
+	private boolean checkObstacleCollision() {
+
+		// TO-DO: code here...
+	}
+	private boolean checkHostileCollision() {
+
+		// TO-DO: code here...
+	}
+	void update() {
+
+		if(checkObstacleCollision() || checkHostileCollision()) {
+
+			// TO-DO: code here...
+		}
+	}
+}
