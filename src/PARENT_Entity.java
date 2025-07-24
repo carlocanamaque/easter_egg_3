2835e2bd0f3f0d5ee367cd9b1bff8d2ef6d5ed35
carlocanamaque <carlocanamaque@proton.me>
2025-07-24 08:20:12 +0800

    Added 'hitbox' variable in PARENT_Entity and PARENT_Obstacle files

--- src/PARENT_Entity.java
+++ src/PARENT_Entity.java
@@ -1,10 +1,12 @@
 package src;
 
 import java.awt.Image;
+import java.awt.Rectangle;
 
 
 public class PARENT_Entity {
 
 	int x, y, speed;
 	Image gif;
+	Rectangle hitbox;
 }
--- src/PARENT_Obstacle.java
+++ src/PARENT_Obstacle.java
@@ -1,11 +1,13 @@
 package src;
 
-
 import java.awt.Image;
+import java.awt.Rectangle;
+
 
 public class PARENT_Obstacle {
 
 	int x, y, width, height;
 	boolean isVertical;
 	Image gif;
+	Rectangle hitbox;
 }
