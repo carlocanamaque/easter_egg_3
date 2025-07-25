102b82b49c92b49cdf05ad54ee26cb8c085c193d
carlocanamaque <carlocanamaque@proton.me>
2025-07-25 14:17:25 +0800

    Added PARENT_Projectile variable in ENTITY_Hostile.java

--- src/ENTITY_Hostile.java
+++ src/ENTITY_Hostile.java
@@ -1,7 +1,16 @@
 package src;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+
 
 public class ENTITY_Hostile extends PARENT_Entity {
 
 	boolean hasProjectile, isEliminated;
+	PARENT_Projectile projectile = new PARENT_Projectile();
+
+	public ENTITY_Hostile() {
+
+		projectile.gif = Toolkit.getDefaultToolkit().createImage("res/hostile_bullet.gif");
+	}
 }
